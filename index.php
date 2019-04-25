1c1c2ea5e3e4933eb69468538ad6ae8731f85ffa
Aleks Yustas <ustin89@mail.ru>
2019-04-25 08:33:49 +0300

    Добавляет функцию форматирования цены лота

--- index.php
+++ index.php
@@ -43,6 +43,28 @@ $lots = [
         'img_url' => 'img/lot-6.jpg'
     ]
 ];
+
+/**
+ * Функция форматирования цены лота
+ *
+ * @param float числовое значение цены лота в исходном формате
+ *
+ * @var string $ruble_letter символ рубля
+ * @var float $pricce_rounded округлённое значение цены лота
+ *
+ * @return string форматированная цена с добавленным символом рубля
+ */
+
+function price_format($price) {
+    $ruble_letter = '<b class="rub">р</b>';
+    $price_rounded = ceil($price);
+
+    if ($price_rounded > 999) {
+        $price_formated = number_format($price_rounded, 0, ',', ' ');
+    }
+
+    return $price_formated . ' ' . $ruble_letter;
+};
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -115,7 +137,7 @@ $lots = [
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?= $lot['price'] ?><b class="rub">р</b></span>
+                                <span class="lot__cost"><?= price_format($lot['price']) ?></span>
                             </div>
                             <div class="lot__timer timer">
                                 12:23
